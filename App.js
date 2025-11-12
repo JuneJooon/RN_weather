9d220c6b7f4cf4da6e30c6ac3f1acc9f5a11f7dd
JuneJooon <junha0305@gmail.com>
2025-11-12 23:26:54 +0900

    style using1

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import React, {useState} from 'react';
 
 export default function App() {
   const [number, setNumber]=useState(0);
+
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 50}}> Number : {number} </Text>
-      <View style={{ flexDirection: "row", gap:10}}>
+      <Text style={styles.text}> Number : {number} </Text>
+      <View style={styles.btnGroup}>
         <Button title="increase" onPress={()=>setNumber(number+1)}/>
         <Button title="decrease" onPress={()=>setNumber(number-1)}/>
       </View>
@@ -22,5 +23,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    //borderWidth: 5,
+    //borderColor : "blue",
   },
+
+  text:{
+    fontSize:50,
+    color: "black",
+    fontWeight:"bold",
+  },
+
+  btnGroup: {
+    flexDirection: "row",
+    gap:10,
+    borderWidth:1,
+    borderColor:"blue",
+    width:200,
+    justifyContent: "center",
+  }
 });
